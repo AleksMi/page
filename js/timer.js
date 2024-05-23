01716080b5dc2333316555277673041ad76ecb30
AleksMi <antistatus@mail.ru>
2024-05-23 23:04:20 +0300

    timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 $(function(){
     function timer(settings){
         var config = {
-            endDate: '2024-05-24 01:00',
+            endDate: '2024-05-27 01:00',
             timeZone: 'Europe/Moscow',
             hours: $('#hours'),
             minutes: $('#minutes'),
